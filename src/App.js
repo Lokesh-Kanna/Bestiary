7d839ee5557960ce283f4b4c71ac12e154607b50
Lokesh-Kanna <lokeshkanna.work@gmail.com>
2021-10-30 14:55:25 +0530

    Added NavBar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Home />
+      </div>
+    </Router>
   );
+}
+
+
+function NavBar() {
+  return(
+    <Router>
+      <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand">Bestiary</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <Link class="nav-link active" to="/">Home <span class="sr-only">(current)</span></Link>
+            <Link class="nav-link" to="/characters">Beasts</Link>
+            <Link class="nav-link" to="/add-beast">Add to Bestiary</Link>
+          </div>
+        </div>
+        </nav>
+      </div>
+    </Router>
+  )
+}
+
+
+function Home() {
+  return (
+    <div>
+      <h3>Hello User,</h3>
+      <img src="" alt="" />
+    </div>
+    
+  )
 }
 
 export default App;
